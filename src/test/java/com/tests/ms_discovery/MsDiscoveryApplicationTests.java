02247d8083f4f5bd6fcaffecc3eb76098e522a7d
ssssshel <angelarteaga17_@outlook.com>
2023-07-14 00:33:09 -0500

    init

---
+++ src/test/java/com/tests/ms_discovery/MsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests.ms_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
